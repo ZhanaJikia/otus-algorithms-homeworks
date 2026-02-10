bc6ea4b3b5a59b23b3b2cf3692c9d996fa3456dc
ZhanaJikia <zhana.jikia.1@btu.edu.ge>
2026-02-10 17:12:54 +0400

    feat: Lucky tickers - junior

--- hw02_lucky_tickets/junior/LuckyTickets.Junior/Program.cs
+++ hw02_lucky_tickets/junior/LuckyTickets.Junior/Program.cs
@@ -1,31 +1,43 @@
 ﻿using System;
 using System.Diagnostics;
 
-internal class Program
-
+namespace LuckyTickets.Junior
 {
-    static void Main()
+    internal class Program
     {
-       var stopwatch = Stopwatch.StartNew();
-       long counter = 0;
+        private const int DigitRange = 10;
 
-       for (int a = 0; a < 10; a++)
-       for (int a1 = 0; a1 < 10; a1++)
-       for (int a2 = 0; a2 < 10; a2++)
-       {
-        int sum = a + a1 + a2;
-        for (int b = 0; b < 10; b++)
-        for (int b1 = 0; b1 < 10; b1++)
+        static void Main()
         {
-            int b3 = sum - b - b1;
-            if (b3 >= 0 && b3 < 10)
-            {
-                counter++;
-            }
+            var stopwatch = Stopwatch.StartNew();
+            long counter = CountLuckyTickets();
+            stopwatch.Stop();
+
+            Console.WriteLine(counter);
+            Console.WriteLine($"Execution time: {stopwatch.ElapsedMilliseconds} ms");
         }
-       }
-        Console.WriteLine(counter);
-        stopwatch.Stop();
-        Console.WriteLine($"Execution time: {stopwatch.ElapsedMilliseconds} ms");
+
+        static long CountLuckyTickets()
+        {
+            long counter = 0;
+
+            for (int a = 0; a < DigitRange; a++)
+            for (int a1 = 0; a1 < DigitRange; a1++)
+            for (int a2 = 0; a2 < DigitRange; a2++)
+            {
+                int sum = a + a1 + a2;
+                for (int b = 0; b < DigitRange; b++)
+                for (int b1 = 0; b1 < DigitRange; b1++)
+                {
+                    int b3 = sum - b - b1;
+                    if (b3 >= 0 && b3 < DigitRange)
+                    {
+                        counter++;
+                    }
+                }
+            }
+
+            return counter;
+        }
     }
 }
